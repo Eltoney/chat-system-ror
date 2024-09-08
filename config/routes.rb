7c7ffb1a4e5046576e6d2f4ba144792c1b60a0f3
Ali Ahmed  Eltoney <aliahmedeltoney@gmail.com>
2024-09-08 21:31:05 +0300

    add routes for the applications

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :applications, only: [:create, :show], param: :token do
+    resources :chats, only: [:create, :index], param: :number do
+      resources :messages, only: [:create, :index] do
+        collection do
+          get 'search', to: 'messages#search'
+        end
+      end
+    end
+  end
 end
