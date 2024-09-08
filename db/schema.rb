811de985d1ba087cebcd6720fdf739b1dec9462e
Ali Ahmed  Eltoney <aliahmedeltoney@gmail.com>
2024-09-08 18:57:12 +0300

    add chat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_08_152816) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_08_155625) do
   create_table "applications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "token"
@@ -19,4 +19,16 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_08_152816) do
     t.datetime "updated_at", null: false
     t.index ["token"], name: "index_applications_on_token", unique: true
   end
+
+  create_table "chats", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "application_id", null: false
+    t.integer "number"
+    t.integer "messages_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["application_id", "number"], name: "index_chats_on_application_id_and_number", unique: true
+    t.index ["application_id"], name: "index_chats_on_application_id"
+  end
+
+  add_foreign_key "chats", "applications"
 end
