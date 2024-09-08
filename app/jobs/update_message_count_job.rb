6e4351880640010b56368832096271f013a1aa39
Ali Ahmed  Eltoney <aliahmedeltoney@gmail.com>
2024-09-08 21:39:03 +0300

    add app jobs

---
+++ app/jobs/update_message_count_job.rb
@@ -0,0 +1,9 @@
+class UpdateMessageCountJob < ApplicationJob
+    queue_as :default
+  
+    def perform(chat_id)
+      chat = Chat.find(chat_id)
+      chat.update(messages_count: chat.messages.count)
+    end
+  end
+  
